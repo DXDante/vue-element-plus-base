a3d020a0c8056f2360b6c05c1a4251ef19abc88f
Dante <dengxiang@mobimedical.cn>
2025-07-22 16:59:56 +0800

    feat: 1. 更改 dynamic-form 组件配置集使用动态异步组件导入、样式异步导入, 提高初次渲染性能

--- src/components/dynamic-form/dynamic-component-map.ts
+++ src/components/dynamic-form/dynamic-component-map.ts
@@ -1,65 +1,118 @@
-import type { Component } from 'vue'
-import { markRaw } from 'vue'
-import {
-  ElAutocomplete,
-  ElCascader,
-  ElCheckboxGroup,
-  ElCheckbox,
-  ElCheckboxButton,
-  ElColorPicker,
-  ElDatePicker,
-  ElInput,
-  ElInputNumber,
-  ElInputTag,
-  ElMention,
-  ElRadioGroup,
-  ElRadio,
-  ElRadioButton,
-  ElRate,
-  ElSelect,
-  ElOptionGroup,
-  ElOption,
-  ElSelectV2,
-  ElSlider,
-  ElSwitch,
-  ElTimePicker,
-  ElTimeSelect,
-  ElTransfer,
-  ElTreeSelect,
-  ElUpload,
-  ElButtonGroup,
-  ElButton
-} from 'element-plus'
+import { defineAsyncComponent, markRaw } from 'vue'
 
 export const dynamicFormConfig = {
-  'el-autocomplete': ElAutocomplete,
-  'el-cascader': ElCascader,
-  'el-checkbox-group': ElCheckboxGroup,
-  'el-checkbox': ElCheckbox,
-  'el-checkbox-button': ElCheckboxButton,
-  'el-color-picker': ElColorPicker,
-  'el-date-picker': ElDatePicker,
-  'el-input': ElInput,
-  'el-input-number': ElInputNumber,
-  'el-input-tag': ElInputTag,
-  'el-mention': ElMention,
-  'el-radio-group': ElRadioGroup,
-  'el-radio': ElRadio,
-  'el-radio-button': ElRadioButton,
-  'el-rate': ElRate,
-  'el-select': ElSelect,
-  'el-option-group': ElOptionGroup,
-  'el-option': ElOption,
-  'el-select-v2': ElSelectV2,
-  'el-slider': ElSlider,
-  'el-switch': ElSwitch,
-  'el-time-picker': ElTimePicker,
-  'el-time-select': ElTimeSelect,
-  'el-transfer': ElTransfer,
-  'el-tree-select': ElTreeSelect,
-  'el-upload': ElUpload,
-  'el-button-group': ElButtonGroup,
-  'el-button': ElButton
+  'el-autocomplete': defineAsyncComponent(() => {
+    import('element-plus/es/components/autocomplete/style/index')
+    return import('element-plus/es/components/autocomplete/index').then((m) => m.default)
+  }),
+  'el-cascader': defineAsyncComponent(() => {
+    import('element-plus/es/components/cascader/style/index')
+    return import('element-plus/es/components/cascader/index').then((m) => m.default)
+  }),
+  'el-checkbox-group': defineAsyncComponent(() => {
+    import('element-plus/es/components/checkbox-group/style/index')
+    return import('element-plus/es/components/checkbox/index').then((m) => m.ElCheckboxGroup)
+  }),
+  'el-checkbox': defineAsyncComponent(() => {
+    import('element-plus/es/components/checkbox/style/index')
+    return import('element-plus/es/components/checkbox/index').then((m) => m.default)
+  }),
+  'el-checkbox-button': defineAsyncComponent(() => {
+    import('element-plus/es/components/checkbox-button/style/index')
+    return import('element-plus/es/components/checkbox/index').then((m) => m.ElCheckboxButton)
+  }),
+  'el-color-picker': defineAsyncComponent(() => {
+    import('element-plus/es/components/color-picker/style/index')
+    return import('element-plus/es/components/color-picker/index').then((m) => m.default)
+  }),
+  'el-date-picker': defineAsyncComponent(() => {
+    import('element-plus/es/components/date-picker/style/index')
+    return import('element-plus/es/components/date-picker/index').then((m) => m.default)
+  }),
+  'el-input': defineAsyncComponent(() => {
+    import('element-plus/es/components/input/style/index')
+    return import('element-plus/es/components/input/index').then((m) => m.default)
+  }),
+  'el-input-number': defineAsyncComponent(() => {
+    import('element-plus/es/components/input-number/style/index')
+    return import('element-plus/es/components/input-number/index').then((m) => m.default)
+  }),
+  'el-input-tag': defineAsyncComponent(() => {
+    import('element-plus/es/components/input-tag/style/index')
+    return import('element-plus/es/components/input-tag/index').then((m) => m.default)
+  }),
+  'el-mention': defineAsyncComponent(() => {
+    import('element-plus/es/components/mention/style/index')
+    return import('element-plus/es/components/mention/index').then((m) => m.default)
+  }),
+  'el-radio-group': defineAsyncComponent(() => {
+    import('element-plus/es/components/radio-group/style/index')
+    return import('element-plus/es/components/radio/index').then((m) => m.ElRadioGroup)
+  }),
+  'el-radio': defineAsyncComponent(() => {
+    import('element-plus/es/components/radio/style/index')
+    return import('element-plus/es/components/radio/index').then((m) => m.default)
+  }),
+  'el-radio-button': defineAsyncComponent(() => {
+    import('element-plus/es/components/radio-button/style/index')
+    return import('element-plus/es/components/radio/index').then((m) => m.ElRadioButton)
+  }),
+  'el-rate': defineAsyncComponent(() => {
+    import('element-plus/es/components/rate/style/index')
+    return import('element-plus/es/components/rate/index').then((m) => m.default)
+  }),
+  'el-select': defineAsyncComponent(() => {
+    import('element-plus/es/components/select/style/index')
+    return import('element-plus/es/components/select/index').then((m) => m.default)
+  }),
+  'el-option-group': defineAsyncComponent(() => {
+    import('element-plus/es/components/option-group/style/index')
+    return import('element-plus/es/components/select/index').then((m) => m.ElOptionGroup)
+  }),
+  'el-option': defineAsyncComponent(() => {
+    import('element-plus/es/components/option/style/index')
+    return import('element-plus/es/components/select/index').then((m) => m.ElOption)
+  }),
+  'el-select-v2': defineAsyncComponent(() => {
+    import('element-plus/es/components/select-v2/style/index')
+    return import('element-plus/es/components/select-v2/index').then((m) => m.default)
+  }),
+  'el-slider': defineAsyncComponent(() => {
+    import('element-plus/es/components/slider/style/index')
+    return import('element-plus/es/components/slider/index').then((m) => m.default)
+  }),
+  'el-switch': defineAsyncComponent(() => {
+    import('element-plus/es/components/switch/style/index')
+    return import('element-plus/es/components/switch/index').then((m) => m.default)
+  }),
+  'el-time-picker': defineAsyncComponent(() => {
+    import('element-plus/es/components/time-picker/style/index')
+    return import('element-plus/es/components/time-picker/index').then((m) => m.default)
+  }),
+  'el-time-select': defineAsyncComponent(() => {
+    import('element-plus/es/components/time-select/style/index')
+    return import('element-plus/es/components/time-select/index').then((m) => m.default)
+  }),
+  'el-transfer': defineAsyncComponent(() => {
+    import('element-plus/es/components/transfer/style/index')
+    return import('element-plus/es/components/transfer/index').then((m) => m.default)
+  }),
+  'el-tree-select': defineAsyncComponent(() => {
+    import('element-plus/es/components/tree-select/style/index')
+    return import('element-plus/es/components/tree-select/index').then((m) => m.default)
+  }),
+  'el-upload': defineAsyncComponent(() => {
+    import('element-plus/es/components/upload/style/index')
+    return import('element-plus/es/components/upload/index').then((m) => m.default)
+  }),
+  'el-button-group': defineAsyncComponent(() => {
+    import('element-plus/es/components/button-group/style/index')
+    return import('element-plus/es/components/button/index').then((m) => m.ElButtonGroup)
+  }),
+  'el-button': defineAsyncComponent(() => {
+    import('element-plus/es/components/button/style/index')
+    return import('element-plus/es/components/button/index').then((m) => m.default)
+  })
 }
 
-export const dynamicFormComponents: Record<string, Component> = markRaw(dynamicFormConfig)
+export const dynamicFormComponents: Record<string, Vue.Component> = markRaw(dynamicFormConfig)
